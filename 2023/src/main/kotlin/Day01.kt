d85c7b84e57059eec2c3c04ab20bd3891bcaa39c
Dan Lew <danlew42@gmail.com>
2023-12-01 09:02:24 -0600

    Solved 2023 - Day 01

--- 2023/src/main/kotlin/Day01.kt
+++ 2023/src/main/kotlin/Day01.kt
@@ -1,5 +1,34 @@
 object Day01 {
+
+  private val NUMBER_REGEX = Regex("\\d")
+  private val NUMBER_AND_TEXT_REGEX = Regex("(?=(one|two|three|four|five|six|seven|eight|nine|\\d))")
+
   fun part1(input: String): Int {
-    return 0
+    return input.splitNewlines()
+      .map { line -> NUMBER_REGEX.findAll(line).map { it.value.toInt() } }
+      .sumCalibrationValues()
   }
+
+  fun part2(input: String): Int {
+    return input.splitNewlines()
+      .map { line ->
+        NUMBER_AND_TEXT_REGEX.findAll(line).map {
+          when (val value = it.groups[1]!!.value) {
+            "one" -> 1
+            "two" -> 2
+            "three" -> 3
+            "four" -> 4
+            "five" -> 5
+            "six" -> 6
+            "seven" -> 7
+            "eight" -> 8
+            "nine" -> 9
+            else -> value.toInt()
+          }
+        }
+      }
+      .sumCalibrationValues()
+  }
+
+  private fun List<Sequence<Int>>.sumCalibrationValues() = this.sumOf { it.first() * 10 + it.last() }
 }
--- 2023/src/test/kotlin/Day01Test.kt
+++ 2023/src/test/kotlin/Day01Test.kt
@@ -1,9 +1,25 @@
 import org.junit.jupiter.api.Test
+import utils.getResourceAsString
 import kotlin.test.assertEquals
 
 class Day01Test {
   @Test
+  fun part1_sample() {
+    assertEquals(142, Day01.part1(getResourceAsString("day01-sample.txt")))
+  }
+
+  @Test
   fun part1() {
-    assertEquals(0, Day01.part1(""))
+    assertEquals(54953, Day01.part1(getResourceAsString("day01.txt")))
+  }
+
+  @Test
+  fun part2_sample() {
+    assertEquals(281, Day01.part2(getResourceAsString("day01-sample2.txt")))
+  }
+
+  @Test
+  fun part2() {
+    assertEquals(53868, Day01.part2(getResourceAsString("day01.txt")))
   }
 }
