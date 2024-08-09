f33227ce5bc27ca5c29239f61413597664799928
Dan Lew <danlew42@gmail.com>
2024-08-09 12:30:11 -0500

    Solved 2015 - Day 06

---
+++ 2015/src/day06.py
@@ -0,0 +1,43 @@
+import re
+
+_PATTERN = re.compile(r"(.*) (\d+),(\d+) through (\d+),(\d+)")
+
+
+def day6_part1(data: str) -> int:
+    grid = [[False] * 1000 for _ in range(1000)]
+    for line in data.splitlines():
+        action, start_x, start_y, end_x, end_y = _PATTERN.match(line).groups()
+        for x in range(int(start_x), int(end_x) + 1):
+            for y in range(int(start_y), int(end_y) + 1):
+                match action:
+                    case "turn on":
+                        grid[y][x] = True
+                    case "toggle":
+                        grid[y][x] = not grid[y][x]
+                    case "turn off":
+                        grid[y][x] = False
+
+    count = 0
+    for row in grid:
+        count += sum([1 if light else 0 for light in row])
+    return count
+
+
+def day6_part2(data: str) -> int:
+    grid = [[0] * 1000 for _ in range(1000)]
+    for line in data.splitlines():
+        action, start_x, start_y, end_x, end_y = _PATTERN.match(line).groups()
+        for x in range(int(start_x), int(end_x) + 1):
+            for y in range(int(start_y), int(end_y) + 1):
+                match action:
+                    case "turn on":
+                        grid[y][x] += 1
+                    case "toggle":
+                        grid[y][x] += 2
+                    case "turn off":
+                        grid[y][x] = max(grid[y][x] - 1, 0)
+
+    count = 0
+    for row in grid:
+        count += sum(row)
+    return count
---
+++ 2015/tests/test_day06.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from src.day06 import day6_part1, day6_part2
+from tests.utils.read_file import read_file
+
+
+class Test(TestCase):
+    def test_day6_part1_sample(self):
+        instructions = [
+            "turn on 0,0 through 999,999",
+            "toggle 0,0 through 999,0",
+            "turn off 499,499 through 500,500"
+        ]
+        self.assertEqual(998_996, day6_part1("\n".join(instructions)))
+
+    def test_day6_part1(self):
+        self.assertEqual(400_410, day6_part1(read_file("inputs/day06.txt")))
+
+    def test_day6_part2_sample(self):
+        instructions = [
+            "turn on 0,0 through 0,0",
+            "toggle 0,0 through 999,999",
+        ]
+        self.assertEqual(2_000_001, day6_part2("\n".join(instructions)))
+
+    def test_day6_part2(self):
+        self.assertEqual(15_343_601, day6_part2(read_file("inputs/day06.txt")))
