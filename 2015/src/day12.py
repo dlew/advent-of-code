0c383f28677644be288ec683e0cdc9db8d23b5c9
Dan Lew <danlew42@gmail.com>
2024-08-12 15:51:11 -0500

    Solved 2015 - Day 12

---
+++ 2015/src/day12.py
@@ -0,0 +1,25 @@
+import json
+
+
+def day12_part1(data: str) -> int:
+    return _traverse(json.loads(data), False)
+
+
+def day12_part2(data: str) -> int:
+    return _traverse(json.loads(data), True)
+
+
+def _traverse(obj: object, ignore_red: bool) -> int:
+    if isinstance(obj, int):
+        return obj
+
+    if isinstance(obj, list):
+        return sum([_traverse(element, ignore_red) for element in obj])
+
+    if isinstance(obj, dict):
+        if ignore_red and 'red' in list(obj.keys()) + list(obj.values()):
+            return 0
+
+        return sum([_traverse(obj[element], ignore_red) for element in obj])
+
+    return 0
---
+++ 2015/tests/test_day12.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+from src.day12 import day12_part1, day12_part2
+from tests.utils.read_file import read_file
+
+
+class Test(TestCase):
+    def test_day12_part1_sample(self):
+        self.assertEqual(6, day12_part1("[1,2,3]"))
+        self.assertEqual(6, day12_part1('{"a":2,"b":4}'))
+        self.assertEqual(3, day12_part1('[[[3]]]'))
+        self.assertEqual(3, day12_part1('{"a":{"b":4},"c":-1}'))
+        self.assertEqual(0, day12_part1('{"a":[-1,1]}'))
+        self.assertEqual(0, day12_part1('[-1,{"a":1}]'))
+        self.assertEqual(0, day12_part1('[]'))
+        self.assertEqual(0, day12_part1('{}'))
+
+    def test_day12_part1(self):
+        self.assertEqual(156366, day12_part1(read_file("inputs/day12.txt")))
+
+    def test_day12_part2_sample(self):
+        self.assertEqual(6, day12_part2("[1,2,3]"))
+        self.assertEqual(4, day12_part2('[1,{"c":"red","b":2},3]'))
+        self.assertEqual(0, day12_part2('{"d":"red","e":[1,2,3,4],"f":5}'))
+        self.assertEqual(6, day12_part2('[1,"red",5]'))
+
+    def test_day12_part2(self):
+        self.assertEqual(96852, day12_part2(read_file("inputs/day12.txt")))
